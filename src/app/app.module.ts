d78720c9142c72a4045648247b7a5ba03fff115d
Herczeg Máté <herczeg6179@gmail.com>
2019-04-09 09:54:42 +0200

    added basic app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
+import { SearchToolComponent } from './search-tool/search-tool.component'
+import { RepoInfoComponent } from './repo-info/repo-info.component'
+import { RepoIssuesComponent } from './repo-issues/repo-issues.component'
+import { AnalyticsComponent } from './analytics/analytics.component'
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        SearchToolComponent,
+        RepoInfoComponent,
+        RepoIssuesComponent,
+        AnalyticsComponent,
+    ],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/repo-issues/repo-issues.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-repo-issues',
+    templateUrl: './repo-issues.component.html',
+    styleUrls: ['./repo-issues.component.scss'],
+})
+export class RepoIssuesComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
