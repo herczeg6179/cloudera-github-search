3855c0d7d43c9887afd29b223a7942b126d34f03
Herczeg Máté <herczeg6179@gmail.com>
2019-04-15 12:25:11 +0200

    added few more test cases

--- src/app/components/authentication/authentication.component.spec.ts
+++ src/app/components/authentication/authentication.component.spec.ts
@@ -1,15 +1,23 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing'
 
 import { AuthenticationComponent } from './authentication.component'
 import { MatBottomSheet } from '@angular/material'
+import { of } from 'rxjs'
+import { delay } from 'rxjs/operators'
 
-describe('AuthenticationComponent', () => {
+const FAKE_ASYNC_TIME = 100
+
+fdescribe('AuthenticationComponent', () => {
     let component: AuthenticationComponent
     let fixture: ComponentFixture<AuthenticationComponent>
 
     beforeEach(async(() => {
+        const bottomSheetMock = {
+            open: () => ({ afterDismissed: () => of(true).pipe(delay(FAKE_ASYNC_TIME)) }),
+        }
+
         TestBed.configureTestingModule({
-            providers: [{ provide: MatBottomSheet, useClass: class MatBottomSheetStub {} }],
+            providers: [{ provide: MatBottomSheet, useValue: bottomSheetMock }],
             declarations: [AuthenticationComponent],
         }).compileComponents()
     }))
@@ -23,4 +31,11 @@ describe('AuthenticationComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy()
     })
+
+    it('should set isAuthenticated to provided value on login form dismiss', fakeAsync(() => {
+        component.openLoginForm()
+        expect(component.isAuthenticated).toEqual(false)
+        tick(FAKE_ASYNC_TIME)
+        expect(component.isAuthenticated).toEqual(true)
+    }))
 })
--- src/app/components/repo-issues/repo-issues.component.spec.ts
+++ src/app/components/repo-issues/repo-issues.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { RepoIssuesComponent } from './repo-issues.component'
 import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { GitIssueInfo } from '@app/modules/api/git-api-interfaces'
 
 describe('RepoIssuesComponent', () => {
     let component: RepoIssuesComponent
@@ -22,5 +23,33 @@ describe('RepoIssuesComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy()
+    })
+
+    it('should set the issuesByState list empty if issues input is empty', () => {
+        component.issues = null
+        component.ngOnChanges()
+        fixture.detectChanges()
+        expect(component.issuesByState).toEqual([])
+    })
+
+    it('should set the issuesByState list if issues input is not empty', () => {
+        const issueStub1 = { id: 1, state: 'open' } as GitIssueInfo
+        const issueStub2 = { id: 2, state: 'open' } as GitIssueInfo
+        const issueStub3 = { id: 3, state: 'closed' } as GitIssueInfo
+
+        component.issues = [issueStub1, issueStub2, issueStub3]
+        component.ngOnChanges()
+        fixture.detectChanges()
+
+        expect(component.issuesByState).toEqual([
+            {
+                state: 'open',
+                issues: [issueStub1, issueStub2],
+            },
+            {
+                state: 'closed',
+                issues: [issueStub3],
+            },
+        ])
     })
 })
